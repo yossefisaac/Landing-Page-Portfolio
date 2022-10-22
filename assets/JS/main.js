4baaa936c9bebfd1a7d094392609d55e28db6517
Yossef Isaac de Oliveira <106195515+yossefisaac@users.noreply.github.com>
2022-10-21 23:18:27 -0300

    Update main.js

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -5,3 +5,10 @@ button.addEventListener('click', () =>{
         scrollTop: $(".portfolio-rolamento").offset().top},
         'slow');
 });
+
+//ScrollReveal
+window.sr = ScrollReveal ({reset: true});
+sr.reveal('.conteudo-principal-escrito-titulo', {duration:1000});
+sr.reveal('.conteudo-principal-escrito-subtitulo', {duration:2000});
+sr.reveal('.conteudo-principal-escrito-botao', {duration:3000});
+sr.reveal('.imagem-cards', {duration:2000});
