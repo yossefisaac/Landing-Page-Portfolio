f1a6e1a360b189c068704f08bd67cf5e9a228dc1
Yossef Isaac de Oliveira <106195515+yossefisaac@users.noreply.github.com>
2022-10-24 04:56:56 +0300

    Update main.js

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -10,4 +10,5 @@ window.sr = ScrollReveal ({reset: true});
 sr.reveal('.conteudo-principal-escrito-titulo', {duration:1000});
 sr.reveal('.conteudo-principal-escrito-subtitulo', {duration:2000});
 sr.reveal('.conteudo-principal-escrito-botao', {duration:3000});
-sr.reveal('.imagem-cards', {duration:2000});
+sr.reveal('.portfolio-title', {duration:2000});
+sr.reveal('.imagem-cards', {duration:3000});
